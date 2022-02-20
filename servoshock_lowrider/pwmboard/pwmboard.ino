571fbffc087021d41932f56bc834ef8f89981ecf
srcs138 <99091740+srcs138@users.noreply.github.com>
2022-02-19 19:18:26 -0500

    Update pwmboard.ino

--- servoshock_lowrider/pwmboard/pwmboard.ino
+++ servoshock_lowrider/pwmboard/pwmboard.ino
@@ -60,7 +60,7 @@ void loop() {
   // Drive each PWM in a 'wave'
   for (uint16_t i=0; i<4096; i += 8) {
     for (uint8_t pwmnum=0; pwmnum < 16; pwmnum++) {
-      pwm.setPWM(pwmnum, 0, (i + (4096/16)*pwmnum) % 4096 );
+      pwm.setPWM(pwmnum, 4096,0);
     }
 #ifdef ESP8266
     yield();  // take a breather, required for ESP8266
